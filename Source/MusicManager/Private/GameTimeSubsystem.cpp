769f256a8b79aa9053f111a2d75b4aae57c67b9e
johdav999 <johandavidsson@hotmail.se>
2025-11-18 18:00:21 +0100

    Add global game time subsystem and integrate artists

---
+++ Source/MusicManager/Private/GameTimeSubsystem.cpp
@@ -0,0 +1,109 @@
+#include "GameTimeSubsystem.h"
+
+#include "Engine/World.h"
+
+UGameTimeSubsystem::UGameTimeSubsystem()
+    : CurrentGameDate(1955, 1, 1)
+    , bIsTimeRunning(false)
+    , bHasReachedSimulationEnd(false)
+{
+}
+
+void UGameTimeSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+
+    CurrentGameDate = FDateTime(1955, 1, 1);
+    bHasReachedSimulationEnd = false;
+    bIsTimeRunning = false;
+
+    StartTimer();
+}
+
+void UGameTimeSubsystem::Deinitialize()
+{
+    StopTimer();
+    Super::Deinitialize();
+}
+
+void UGameTimeSubsystem::AdvanceMonth()
+{
+    check(IsInGameThread());
+
+    if (HasSimulationEnded())
+    {
+        StopTimer();
+        return;
+    }
+
+    int32 NewYear = CurrentGameDate.GetYear();
+    int32 NewMonth = CurrentGameDate.GetMonth() + 1;
+
+    if (NewMonth > 12)
+    {
+        NewMonth = 1;
+        ++NewYear;
+    }
+
+    if (NewYear > 2026)
+    {
+        bHasReachedSimulationEnd = true;
+        StopTimer();
+        return;
+    }
+
+    CurrentGameDate = FDateTime(NewYear, NewMonth, 1);
+
+    OnMonthAdvanced.Broadcast(CurrentGameDate);
+}
+
+void UGameTimeSubsystem::PauseTime(bool bPause)
+{
+    check(IsInGameThread());
+
+    if (bPause)
+    {
+        if (bIsTimeRunning)
+        {
+            StopTimer();
+        }
+    }
+    else
+    {
+        StartTimer();
+    }
+}
+
+void UGameTimeSubsystem::StartTimer()
+{
+    if (HasSimulationEnded())
+    {
+        return;
+    }
+
+    if (UWorld* World = GetWorld())
+    {
+        World->GetTimerManager().SetTimer(TimeAdvanceHandle, this, &UGameTimeSubsystem::AdvanceMonth, 10.0f, true);
+        bIsTimeRunning = true;
+    }
+}
+
+void UGameTimeSubsystem::StopTimer()
+{
+    if (UWorld* World = GetWorld())
+    {
+        World->GetTimerManager().ClearTimer(TimeAdvanceHandle);
+    }
+
+    bIsTimeRunning = false;
+}
+
+bool UGameTimeSubsystem::HasSimulationEnded() const
+{
+    if (bHasReachedSimulationEnd)
+    {
+        return true;
+    }
+
+    return CurrentGameDate.GetYear() > 2026;
+}
