e19b6c2c4ace574759bc56ca2ac9693af0da1a42
johdav999 <johandavidsson@hotmail.se>
2025-12-13 17:40:51 +0100

    Add editable style properties to region map button

--- Source/MusicManager/Private/UI/RegionMapButton.cpp
+++ Source/MusicManager/Private/UI/RegionMapButton.cpp
@@ -14,6 +14,8 @@ void URegionMapButton::NativeConstruct()
     {
         RegionText->SetText(FText::FromString(RegionId));
     }
+
+    ApplyStyle();
 }
 
 void URegionMapButton::InitializeRegion(const FString& InRegionId)
@@ -24,9 +26,82 @@ void URegionMapButton::InitializeRegion(const FString& InRegionId)
     {
         RegionText->SetText(FText::FromString(RegionId));
     }
+
+    ApplyStyle();
 }
 
 void URegionMapButton::HandleClicked()
 {
     OnRegionClicked.Broadcast(RegionId);
+}
+
+void URegionMapButton::ApplyStyle()
+{
+    if (RegionButton)
+    {
+        FButtonStyle NewStyle = RegionButton->WidgetStyle;
+        NewStyle.Normal.TintColor = FSlateColor(ButtonColor);
+        NewStyle.Hovered.TintColor = FSlateColor(HoverColor);
+        RegionButton->SetStyle(NewStyle);
+    }
+
+    if (RegionText)
+    {
+        RegionText->SetColorAndOpacity(TextColor);
+
+        FSlateFontInfo FontInfo = RegionText->GetFont();
+        FontInfo.Size = FontSize;
+        RegionText->SetFont(FontInfo);
+    }
+
+    if (RootSizeBox)
+    {
+        if (ButtonSize.X > 0.f)
+        {
+            RootSizeBox->SetWidthOverride(ButtonSize.X);
+        }
+        else
+        {
+            RootSizeBox->ClearWidthOverride();
+        }
+
+        if (ButtonSize.Y > 0.f)
+        {
+            RootSizeBox->SetHeightOverride(ButtonSize.Y);
+        }
+        else
+        {
+            RootSizeBox->ClearHeightOverride();
+        }
+    }
+}
+
+void URegionMapButton::SetButtonColor(const FLinearColor& InColor)
+{
+    ButtonColor = InColor;
+    ApplyStyle();
+}
+
+void URegionMapButton::SetHoverColor(const FLinearColor& InColor)
+{
+    HoverColor = InColor;
+    ApplyStyle();
+}
+
+void URegionMapButton::SetTextColor(const FLinearColor& InColor)
+{
+    TextColor = InColor;
+    ApplyStyle();
+}
+
+void URegionMapButton::SetFontSize(int32 InSize)
+{
+    FontSize = InSize;
+    ApplyStyle();
+}
+
+void URegionMapButton::SetButtonSize(const FVector2D& InSize)
+{
+    ButtonSize = InSize;
+    ApplyStyle();
 }
--- Source/MusicManager/Public/UI/RegionMapButton.h
+++ Source/MusicManager/Public/UI/RegionMapButton.h
@@ -3,6 +3,7 @@
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "Components/Button.h"
+#include "Components/SizeBox.h"
 #include "Components/TextBlock.h"
 #include "RegionMapButton.generated.h"
 
@@ -19,6 +20,26 @@ class MUSICMANAGER_API URegionMapButton : public UUserWidget
 public:
     virtual void NativeConstruct() override;
 
+    /** Base fill color for the button. */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Style")
+    FLinearColor ButtonColor = FLinearColor::White;
+
+    /** Hover color for the button. */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Style")
+    FLinearColor HoverColor = FLinearColor::White;
+
+    /** Color applied to the button text. */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Style")
+    FLinearColor TextColor = FLinearColor::White;
+
+    /** Font size for the button text. */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Style")
+    int32 FontSize = 24;
+
+    /** Optional button size applied when a root SizeBox is present. */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Style")
+    FVector2D ButtonSize = FVector2D::ZeroVector;
+
     /** Optional button bound in the designer. */
     UPROPERTY(meta = (BindWidgetOptional))
     UButton* RegionButton;
@@ -27,6 +48,10 @@ public:
     UPROPERTY(meta = (BindWidgetOptional))
     UTextBlock* RegionText;
 
+    /** Optional root size box to control the button dimensions. */
+    UPROPERTY(meta = (BindWidgetOptional))
+    USizeBox* RootSizeBox;
+
     /** Broadcast when this region button is clicked. */
     UPROPERTY(BlueprintAssignable, Category = "RegionMap")
     FOnRegionClicked OnRegionClicked;
@@ -34,6 +59,30 @@ public:
     /** Initialize the region id and update the text. */
     UFUNCTION(BlueprintCallable, Category = "Region")
     void InitializeRegion(const FString& InRegionId);
+
+    /** Apply all configured style properties to the underlying widgets. */
+    UFUNCTION(BlueprintCallable, Category = "Style")
+    void ApplyStyle();
+
+    /** Set the button base color and update the style. */
+    UFUNCTION(BlueprintCallable, Category = "Style")
+    void SetButtonColor(const FLinearColor& InColor);
+
+    /** Set the button hover color and update the style. */
+    UFUNCTION(BlueprintCallable, Category = "Style")
+    void SetHoverColor(const FLinearColor& InColor);
+
+    /** Set the text color and update the style. */
+    UFUNCTION(BlueprintCallable, Category = "Style")
+    void SetTextColor(const FLinearColor& InColor);
+
+    /** Set the text font size and update the style. */
+    UFUNCTION(BlueprintCallable, Category = "Style")
+    void SetFontSize(int32 InSize);
+
+    /** Set the desired button size and update the style. */
+    UFUNCTION(BlueprintCallable, Category = "Style")
+    void SetButtonSize(const FVector2D& InSize);
 
     FString GetRegionId() const { return RegionId; }
 
